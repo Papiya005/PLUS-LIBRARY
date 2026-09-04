1e726ea705d3154b522684fb2391847a6d20dfe6
papiya dutta <papiyadutta448@gmail.com>
2026-09-04 11:11:09 +0530

    find missing in ways

--- Missingno.java
+++ Missingno.java
@@ -1,5 +1,16 @@
 import java.util.*;
 public class Missingno{
+    //another try
+    public static int findmiss(int nums[]){
+        int actualVal=nums.length*(nums.length+1)/2;
+        int currVal=0;
+        for(int i=0;i<nums.length;i++){
+          currVal=currVal+nums[i];
+
+        }
+        int number=actualVal-currVal;
+        return number;
+    }
     public static int findMissing(int nums[]){
         
         int x_or=nums.length;
@@ -16,5 +27,6 @@ public class Missingno{
         nums[2]=1;
         System.out.println(Arrays.toString(nums));
         System.out.println(findMissing(nums));
+        System.out.println(findmiss(nums));
     }
 }
